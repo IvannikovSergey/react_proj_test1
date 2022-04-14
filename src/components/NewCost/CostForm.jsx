41a335bc75220acf67dade8d1eae260b09fa86e3
Sergey <nagoymail@gmail.com>
2022-04-14 17:44:29 +0500

    Прослушивание ввода пользователя

--- src/components/NewCost/CostForm.jsx
+++ src/components/NewCost/CostForm.jsx
@@ -1,23 +1,56 @@
 import './CostForm.css'
+import {useState} from "react";
 const CostForm = () => {
 
-    const changeInputHandler = (e) => {
-        console.log(e.target.value)
+    // const [name, setName] = useState('')
+    // const [amount, setAmount] = useState('')
+    // const [date, setDate] = useState('2019-12-12')
+
+    const [userInput, setUserInput] = useState({
+        name:'',
+        amount:'',
+        date:''
+    })
+
+    const nameChangeHandler = (e) => {
+        // setName(e.target.value)
+        setUserInput({
+            ...userInput,
+            name: e.target.value
+
+        })
+    }
+    const amountChangeHandler = (e) => {
+        // setAmount(e.target.value)
+        setUserInput({
+            ...userInput,
+            amount: e.target.value
+
+        })
+
+    }
+    const dateChangeHandler = (e) => {
+      // setDate(e.target.value)
+        setUserInput({
+            ...userInput,
+            date: e.target.value
+
+        })
     }
 
     return <form>
         <div className='new-cost__control'>
             <div className='new-cost__control'>
                 <label>Название</label>
-                <input onChange={changeInputHandler} type="text"/>
+                <input onChange={nameChangeHandler} type="text"/>
             </div>
             <div className='new-cost__control'>
                 <label>Сумма</label>
-                <input type="number" min='0.01' step='0.01'/>
+                <input onChange={amountChangeHandler} type="number" min='0.01' step='0.01'/>
             </div>
             <div className='new-cost__control'>
                 <label>Дата</label>
-                <input type="date" step='2019-01-01'/>
+                <input onChange={dateChangeHandler} type="date" step='2019-01-01'/>
             </div>
         </div>
         <div >
